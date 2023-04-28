c78bc6519452d131017858f887f5b5a02ef4abe4
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-04-28 19:44:59 +0200

    feat: Qt5 GUI

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from Scripts.generator import * 
+from Scripts.detection import *
+from Scripts.visualize_dataset import *
+
+class ClusteringRingsGUI(QMainWindow):
+    def __init__(self):
+        super(ClusteringRingsGUI, self).__init__()
+        uic.loadUi("clustering_rings.ui", self)
+        self.setFixedSize(self.size())
+
+        self.generate_button.clicked.connect(lambda: generate(self.num_circs_input.text(), 
+                                                              self.num_instances_input.text(),
+                                                              self.randomness_input.text(),
+                                                              self.range_radius_input.text(),
+                                                              self.range_points_input.text(),
+                                                              self.noise_ratio_input.text(),
+                                                              self.dataset_input_generation.text()))
+
+        self.detect_button.clicked.connect(lambda: detect(self.dataset_input_detection.text(), 
+                                                            self.results_input_detection.text(),
+                                                            self.fuzziness_input.text(),
+                                                            self.attempts_input.text(),
+                                                            self.max_iter_input.text(),
+                                                            self.mem_thress_input.text()))
+        
+        self.vis_dataset_button.clicked.connect(lambda: init_vis(self.dataset_input_visuals.text()))
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    main_window = ClusteringRingsGUI()
+    main_window.show()
+    app.exec_()
